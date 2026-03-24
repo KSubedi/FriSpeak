83952ffc7669f24a34113973765a4e6eec4a39dc
Kaushal Subedi <kaushal@wireshock.com>
2026-03-23 21:07:35 -0500

    Initial Commit

---
+++ FriSpeak/FriSpeakApp.swift
@@ -0,0 +1,21 @@
+//
+//  FriSpeakApp.swift
+//  FriSpeak
+//
+//  Created by Kaushal Subedi on 3/23/26.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct FriSpeakApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
